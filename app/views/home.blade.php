598fb12adfbeb80dbb016a4320841d9837e0e70a
Meet Godhani <meet_godhani1@yahoo.com>
2014-07-10 22:22:39 -0400

    update

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,6 +3,22 @@
 <div class="col-md-12 shortlink-form">
     <div class="row">
         <h2 class="text-center">Generate Short link</h2>
+        <br/>
+        <br/>
+        <div class="center-block">
+            <form class="form">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="url" placeholder="Paste your link here ..." class="form-control">
+                              <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">Shrink !</button>
+                              </span>
+                        </div><!-- /input-group -->
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @stop
